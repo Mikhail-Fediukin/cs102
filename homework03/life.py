b7b8f12a9015bc0b94b9af34bda687281258296c
Mikhail Fediukin <90192527+Mikhail-Fediukin@users.noreply.github.com>
2021-12-30 23:53:56 +0300

    Add files via upload

--- homework03/life.py
+++ homework03/life.py
@@ -1,6 +1,7 @@
 import pathlib
 import random
 import typing as tp
+from pprint import pp
 
 import pygame
 from pygame.locals import *
@@ -29,46 +30,81 @@ class GameOfLife:
         self.generations = 1
 
     def create_grid(self, randomize: bool = False) -> Grid:
-        # Copy from previous assignment
-        pass
+        if randomize:
+            return [[random.choice([1, 0]) for i in range(self.cols)] for j in range(self.rows)]
+        else:
+            return [[0 for i in range(self.cols)] for j in range(self.rows)]
 
     def get_neighbours(self, cell: Cell) -> Cells:
-        # Copy from previous assignment
-        pass
+        cells = []
+        x = cell[0]
+        y = cell[1]
+        if y - 1 >= 0:
+            cells.append(self.curr_generation[y - 1][x])
+        if y + 1 < self.rows:
+            cells.append(self.curr_generation[y + 1][x])
+        if x - 1 >= 0:
+            if y - 1 >= 0:
+                cells.append(self.curr_generation[y - 1][x - 1])
+            cells.append(self.curr_generation[y][x - 1])
+            if y + 1 < self.rows:
+                cells.append(self.curr_generation[y + 1][x - 1])
+        if x + 1 < self.cols:
+            if y - 1 >= 0:
+                cells.append(self.curr_generation[y - 1][x + 1])
+            cells.append(self.curr_generation[y - 1][x + 1])
+            if y + 1 < self.rows:
+                cells.append(self.curr_generation[y + 1][x + 1])
+        return cells
 
     def get_next_generation(self) -> Grid:
-        # Copy from previous assignment
-        pass
+        updated_grid = []
+        for y in range(0, self.rows):
+            row = []
+            for x in range(0, self.cols):
+                if sum(self.get_neighbours((x, y))) == 3 or (
+                    self.curr_generation[y][x] == 1 and sum(self.get_neighbours((x, y))) == 2
+                ):
+                    row.append(1)
+                else:
+                    row.append(0)
+            updated_grid.append(row)
+        return updated_grid
 
     def step(self) -> None:
-        """
-        Выполнить один шаг игры.
-        """
-        pass
+        if not self.is_max_generations_exceeded and self.is_changing:
+            self.prev_generation = self.curr_generation
+            self.curr_generation = self.get_next_generation()
+            self.generations += 1
 
     @property
     def is_max_generations_exceeded(self) -> bool:
-        """
-        Не превысило ли текущее число поколений максимально допустимое.
-        """
-        pass
+        if self.max_generations is not None:
+            return self.generations >= self.max_generations
+        else:
+            return False
 
     @property
     def is_changing(self) -> bool:
-        """
-        Изменилось ли состояние клеток с предыдущего шага.
-        """
-        pass
+        return self.curr_generation != self.prev_generation
 
     @staticmethod
     def from_file(filename: pathlib.Path) -> "GameOfLife":
-        """
-        Прочитать состояние клеток из указанного файла.
-        """
-        pass
+        file = open(filename, "r")
+        file_grid = file.read().split()
+        for i, row in enumerate(file_grid):
+            file_grid[i] = list(map(int, list(row)))
+        grid = GameOfLife((len(file_grid), len(file_grid[0])))
+        grid.curr_generation = file_grid
+        return grid
 
     def save(self, filename: pathlib.Path) -> None:
-        """
-        Сохранить текущее состояние клеток в указанный файл.
-        """
-        pass
+        file = open(filename, "w")
+        for i in self.curr_generation:
+            file.write("".join(list(map(str, i))) + "\n")
+        file.close()
+
+
+if __name__ == "__main__":
+    life0 = GameOfLife.from_file(pathlib.Path("glider.txt"))
+    pp(life0.curr_generation)
